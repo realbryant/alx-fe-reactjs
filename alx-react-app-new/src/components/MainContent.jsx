f332d38c5f202b50313e2ece3ed6829648dc7d3b
realbryant <realbryant.b@gmail,com>
2025-09-07 18:15:43 +0300

    update

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f9f9f9', minHeight: '200px' }}>
       <h2 style={{ color: 'darkred' }}>Welcome to My Page!</h2>
-      <p style={{ fontSize: '18px' }}>Here you can learn more about my favorite cities and why I love them.</p>
+      <p style={{ fontSize: '18px' }}>Here you can learn more about my favorite cities and why I love them  I love to visit New York, Paris, and Tokyo..</p>
     </main>
   );
 }
