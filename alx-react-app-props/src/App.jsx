f893f355bf68f20dfefa24d86691e8769a7dc85f
realbryant <realbryant.b@gmail,com>
2025-09-07 19:35:29 +0300

    rename folder

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,58 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import ProfilePage from './ProfilePage';
+import UserInfo from "./UserInfo";
+import UserContext from "./UserContext";
+import UserDetails from "./UserDetails";  
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <>
+  
+      <div>
+        <WelcomeMessage />,
+        <Header />,
+        <MainContent />,
+        <Footer />
+        <UserContext.Provider value={userData}>
+      <ProfilePage />
+        </UserContext.Provider>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <ProfilePage userData={userData} />;
+      </div>
+
+      <div className="logos"> 
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+
+
+export default Footer;
