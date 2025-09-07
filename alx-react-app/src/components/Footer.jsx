9239d746cb760411f722c7d06cc33a406bd45b28
realbryant <realbryant.b@gmail,com>
2025-09-07 16:04:47 +0300

    update

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,13 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+
+
+export default Footer;
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,9 @@
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+function MainContent() {
+    return (
+        <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
