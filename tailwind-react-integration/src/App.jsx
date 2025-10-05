589d173c54fc247aca6db429189c75311dc5e299
realbryant <realbryant.b@gmail,com>
2025-10-05 16:27:38 +0300

    update

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div>
+      <UserProfile />
+    </div>
     <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
       Tailwind is Working! 🚀
     </div>
