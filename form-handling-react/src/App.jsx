2922a12b5c2859a4e80d81f9ac038229522606d8
realbryant <realbryant.b@gmail,com>
2025-10-13 13:32:45 +0300

    update

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-import <RegistrationForm
-import FormikForm from './components/FormikForm'
 import RegistrationForm from './components/RegistrationForm'
+import FormikForm from './components/FormikForm'
+
 
 
 function App() {
