b56098af4053fb65d495ad4c2cf35850d78469f7
realbryant <realbryant.b@gmail,com>
2025-10-13 13:13:16 +0300

    UPDATE

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RegistrationForm from './components/RegistrationForm'
+import <RegistrationForm
 import FormikForm from './components/FormikForm'
+import RegistrationForm from './components/RegistrationForm'
+
 
 function App() {
   const [count, setCount] = useState(0)
